3ccf66e6374d7f7a034300a1210d5261420fe834
theda <melany_nadine@hotmail.com>
2025-02-15 20:43:28 -0500

    done!

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const BOOKS_LIST = [
-    {"author": "Michel Houelebellbecq", "title": "Amplificación del campo de batalla", "imgSrc": "", "price": "$mmm"},
-    {"author": "Huilo Ruales", "title": "Edén y Eva", "imgSrc": "", "price": "$mmm"},
-    {"author": "Tomás Eloy Martínez", "title": "Purgatorio", "imgSrc": "", "price": "$mmm"},
-    {"author": "José Saramago", "title": "El evangelio según Jesucristo", "imgSrc": "", "price": "$mmm"},
-    {"author": "Throton Wilder", "title": "Los idus de marzo", "imgSrc": "", "price": "$mmm"},
-    {"author": "Anton Chejov", "title": "El beso y otros cuentos", "imgSrc": "", "price": "$mmm"},
-    {"author": "Nicolai Gogol", "title": "Cuentos completos", "imgSrc": "", "price": "$mmm"},
-    {"author": "Homero", "title": "La odisea", "imgSrc": "", "price": "$mmm"},
-    {"author": "Homero", "title": "La iliada", "imgSrc": "", "price": "$mmm"},
-    {"author": "Goethe", "title": "Fausto", "imgSrc": "", "price": "$mmm"},
-    {"author": "Ernest Hemingway", "title": "El viejo y el mar", "imgSrc": "", "price": "$mmm"},
-    {"author": "Jorge Luis Borges", "title": "Los conjurados", "imgSrc": "", "price": "$mmm"},
-    {"author": "Carlos Cuahutémoc Sánchez", "title": "Un grito desesperado", "imgSrc": "", "price": "$mmm"},
-    {"author": "Paulo Coelho", "title": "El alquimista", "imgSrc": "", "price": "$mmm"},
-    {"author": "Herman Hesse", "title": "Siddhartha", "imgSrc": "", "price": "$mmm"},
-    {"author": "Alessandro Baricco", "title": "Seda", "imgSrc": "", "price": "$mmm"},
-    {"author": "Virgilio", "title": "Eneida", "imgSrc": "", "price": "$mmm"},
-    {"author": "James Joyce", "title": "Dublineses", "imgSrc": "", "price": "$mmm"},
-    {"author": "Mario Vargas Llosa", "title": "Pantaleón y las visitadoras", "imgSrc": "", "price": "$mmm"},
-    {"author": "Mario Vargas Llosa", "title": "La ciudad y los perros", "imgSrc": "", "price": "$mmm"},
-    {"author": "Yokio Mishima", "title": "La corrupción del ángel", "imgSrc": "", "price": "$mmm"},
-    {"author": "Juan Carlos Onetti", "title": "Juntacadáveres", "imgSrc": "", "price": "$mmm"},
-    {"author": "Gabriel García Marquez", "title": "Doce cuentos peregrinos", "imgSrc": "", "price": "$mmm"},
-    {"author": "Gabriel García Marquez", "title": "El general en su laberinto", "imgSrc": "", "price": "$mmm"},
-    {"author": "Günter Grass", "title": "El tambor de ojalata", "imgSrc": "", "price": "$mmm"},
-    {"author": "Agatha Christie", "title": "Los elefantes pueden recordar y otros relatos", "imgSrc": "", "price": "$mmm"},
-    {"author": "Orhan Pamuk", "title": "Estambul", "imgSrc": "", "price": "$mmm"},
-    {"author": "Franz Kafka", "title": "El proceso", "imgSrc": "", "price": "$mmm"},
-    {"author": "Elias Canetti", "title": "Auto de fe", "imgSrc": "", "price": "$mmm"}
+    {"author": "Michel Houelebellbecq", "title": "Amplificación del campo de batalla", "imgSrc": "amplificacion.jpg", "price": "$8.00"},
+    {"author": "Huilo Ruales", "title": "Edén y Eva", "imgSrc": "eden_y_eva.jpg", "price": "$10.00"},
+    {"author": "Tomás Eloy Martínez", "title": "Purgatorio", "imgSrc": "purgatorio.jpg", "price": "$12.00"},
+    {"author": "José Saramago", "title": "El evangelio según Jesucristo", "imgSrc": "el_evangelio.jpg", "price": "$14.00"},
+    {"author": "Throton Wilder", "title": "Los idus de marzo", "imgSrc": "los_idus.jpg", "price": "$8.00"},
+    {"author": "Anton Chejov", "title": "El beso y otros cuentos", "imgSrc": "el_beso.jpg", "price": "$6.00"},
+    {"author": "Nicolai Gogol", "title": "Cuentos completos", "imgSrc": "cuentos.jpg", "price": "$6.00"},
+    {"author": "Homero", "title": "La odisea", "imgSrc": "odisea.jpg", "price": "$10.00"},
+    {"author": "Homero", "title": "La iliada", "imgSrc": "iliada.jpg", "price": "$7.00"},
+    {"author": "Goethe", "title": "Fausto", "imgSrc": "fausto.jpg", "price": "$11.00"},
+    {"author": "Ernest Hemingway", "title": "El viejo y el mar", "imgSrc": "viejo.jpg", "price": "$6.00"},
+    {"author": "Jorge Luis Borges", "title": "Los conjurados", "imgSrc": "conjurados.jpg", "price": "$6.00"},
+    {"author": "Carlos Cuahutémoc Sánchez", "title": "Un grito desesperado", "imgSrc": "grito.jpg", "price": "$6.00"},
+    {"author": "Paulo Coelho", "title": "El alquimista", "imgSrc": "alquimista.jpg", "price": "$6.00"},
+    {"author": "Herman Hesse", "title": "Siddhartha", "imgSrc": "siddhartha.jpg", "price": "$12.00"},
+    {"author": "Alessandro Baricco", "title": "Seda", "imgSrc": "seda.jpg", "price": "$8.00"},
+    {"author": "Virgilio", "title": "Eneida", "imgSrc": "eneida.jpg", "price": "$10.00"},
+    {"author": "James Joyce", "title": "Dublineses", "imgSrc": "dublineses.jpg", "price": "$7.00"},
+    {"author": "Mario Vargas Llosa", "title": "Pantaleón y las visitadoras", "imgSrc": "pantaleon.jpg", "price": "$12.00"},
+    {"author": "Mario Vargas Llosa", "title": "La ciudad y los perros", "imgSrc": "ciudad.jpg", "price": "$14.00"},
+    {"author": "Yokio Mishima", "title": "La corrupción del ángel", "imgSrc": "corrupcion.jpg", "price": "$12.00"},
+    {"author": "Juan Carlos Onetti", "title": "Juntacadáveres", "imgSrc": "juntacadaveres.jpg", "price": "$15.00"},
+    {"author": "Gabriel García Marquez", "title": "Doce cuentos peregrinos", "imgSrc": "doce_cuentos.jpg", "price": "$12.00"},
+    {"author": "Gabriel García Marquez", "title": "El general en su laberinto", "imgSrc": "general.jpg", "price": "$13.00"},
+    {"author": "Günter Grass", "title": "El tambor de ojalata", "imgSrc": "tambor.jpg", "price": "$15.00"},
+    {"author": "Agatha Christie", "title": "Los elefantes pueden recordar y otros relatos", "imgSrc": "elefantes.jpg", "price": "$10.00"},
+    {"author": "Orhan Pamuk", "title": "Estambul", "imgSrc": "estambul.jpg", "price": "$17.00"},
+    {"author": "Franz Kafka", "title": "El proceso", "imgSrc": "proceso.jpg", "price": "$13.00"},
+    {"author": "Elias Canetti", "title": "Auto de fe", "imgSrc": "auto.jpg", "price": "$10.00"}
 ]
 
 var booksList;
@@ -39,7 +39,7 @@ const setBooksList = list => {
 setBooksList(BOOKS_LIST);
 
 const createNode = (title, author, imgSrc, price) => {
-    let node = `<div class="book"><img src="${imgSrc}"> <span><b>Autor:&nbsp;</b> <p>${author}</p></span> <span><b>Título:&nbsp;</b> <p>${title}</p></span> <b class="price">${price}</b></div>`;
+    let node = `<div class="book"><img src="img/${imgSrc}"> <span><b>Autor:&nbsp;</b> <p>${author}</p></span> <span><b>Título:&nbsp;</b> <p>${title}</p></span> <b class="price">${price}</b></div>`;
     return node;
 }
 
